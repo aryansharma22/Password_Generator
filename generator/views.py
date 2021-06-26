c79ae445a0e3aab3831b80655072d2d89790c3ae
aryansharma22 <aryanafgji@gmail.com>
2021-06-26 22:14:06 +0530

    third

--- generator/views.py
+++ generator/views.py
@@ -4,7 +4,7 @@ import random
 
 # Create your views here.
 def home(request):
-    return render(request , 'generator/index.html')
+    return render(request , 'generator/home.html')
 
 def about(request):
     return render(request , 'generator/about.html')    
